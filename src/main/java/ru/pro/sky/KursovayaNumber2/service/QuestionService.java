873ed06faafa8f6a893727cbfa6e0819bef907fc
Grisha <guruwolf8@gmail.com>
2023-09-12 10:21:51 +0300

    Start

---
+++ src/main/java/ru/pro/sky/KursovayaNumber2/service/QuestionService.java
@@ -0,0 +1,17 @@
+package ru.pro.sky.KursovayaNumber2.service;
+
+import ru.pro.sky.KursovayaNumber2.dto.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
---
+++ src/test/java/ru/pro/sky/KursovayaNumber2/KursovayaNumber2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.KursovayaNumber2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayaNumber2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
